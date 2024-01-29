0faf670b180c6b0415424708d166ed17a311f99a
Jake Davies <jgdavies24@gmail.com>
2024-01-29 21:42:57 +1000

    voxel working now

--- src/voxel.rs
+++ src/voxel.rs
@@ -6,112 +6,42 @@ use wgpu::util::DeviceExt;
 use crate::{model, texture};
 
 pub async fn load_block(device: &wgpu::Device, queue: &wgpu::Queue) -> anyhow::Result<Model> {
-    let length = 1.0;
-    let width = 1.0;
-    let height = 1.0;
     let vertices = [
         // Top
-        ModelVertex {
-            position: [0.0, height, 0.0],
-            normal: [0.0, 1.0, 0.0],
-        },
-        ModelVertex {
-            position: [width, height, 0.0],
-            normal: [0.0, 1.0, 0.0],
-        },
-        ModelVertex {
-            position: [width, height, length],
-            normal: [0.0, 1.0, 0.0],
-        },
-        ModelVertex {
-            position: [0.0, height, length],
-            normal: [0.0, 1.0, 0.0],
-        },
+        ModelVertex::new([1.0, 1.0, 1.0], [0.0, 1.0, 0.0]),
+        ModelVertex::new([1.0, 1.0, -1.0], [0.0, 1.0, 0.0]),
+        ModelVertex::new([-1.0, 1.0, -1.0], [0.0, 1.0, 0.0]),
+        ModelVertex::new([-1.0, 1.0, 1.0], [0.0, 1.0, 0.0]),
+
         // Bottom
-        ModelVertex {
-            position: [0.0, 0.0, 0.0],
-            normal: [0.0, -1.0, 0.0],
-        },
-        ModelVertex {
-            position: [width, 0.0, 0.0],
-            normal: [0.0, -1.0, 0.0],
-        },
-        ModelVertex {
-            position: [width, 0.0, length],
-            normal: [0.0, -1.0, 0.0],
-        },
-        ModelVertex {
-            position: [0.0, 0.0, length],
-            normal: [0.0, -1.0, 0.0],
-        },
+        ModelVertex::new([-1.0, -1.0, 1.0], [0.0, -1.0, 0.0]),
+        ModelVertex::new([-1.0, -1.0, -1.0], [0.0, -1.0, 0.0]),
+        ModelVertex::new([1.0, -1.0, -1.0], [0.0, -1.0, 0.0]),
+        ModelVertex::new([1.0, -1.0, 1.0], [0.0, -1.0, 0.0]),
+        
         // Front
-        ModelVertex {
-            position: [0.0, 0.0, length],
-            normal: [0.0, 0.0, 1.0],
-        },
-        ModelVertex {
-            position: [width, 0.0, length],
-            normal: [0.0, 0.0, 1.0],
-        },
-        ModelVertex {
-            position: [width, height, length],
-            normal: [0.0, 0.0, 1.0],
-        },
-        ModelVertex {
-            position: [0.0, height, length],
-            normal: [0.0, 0.0, 1.0],
-        },
+        ModelVertex::new([1.0, 1.0, 1.0], [0.0, 0.0, 1.0]),
+        ModelVertex::new([-1.0, 1.0, 1.0], [0.0, 0.0, 1.0]),
+        ModelVertex::new([-1.0, -1.0, 1.0], [0.0, 0.0, 1.0]),
+        ModelVertex::new([1.0, -1.0, 1.0], [0.0, 0.0, 1.0]),
+
         // Back
-        ModelVertex {
-            position: [0.0, 0.0, 0.0],
-            normal: [0.0, 0.0, -1.0],
-        },
-        ModelVertex {
-            position: [width, 0.0, 0.0],
-            normal: [0.0, 0.0, -1.0],
-        },
-        ModelVertex {
-            position: [width, height, 0.0],
-            normal: [0.0, 0.0, -1.0],
-        },
-        ModelVertex {
-            position: [0.0, height, 0.0],
-            normal: [0.0, 0.0, -1.0],
-        },
+        ModelVertex::new([1.0, -1.0, -1.0], [0.0, 0.0, -1.0]),
+        ModelVertex::new([-1.0, -1.0, -1.0], [0.0, 0.0, -1.0]),
+        ModelVertex::new([-1.0, 1.0, -1.0], [0.0, 0.0, -1.0]),
+        ModelVertex::new([1.0, 1.0, -1.0], [0.0, 0.0, -1.0]),
+
         // Left
-        ModelVertex {
-            position: [0.0, 0.0, 0.0],
-            normal: [-1.0, 0.0, 0.0],
-        },
-        ModelVertex {
-            position: [0.0, 0.0, length],
-            normal: [-1.0, 0.0, 0.0],
-        },
-        ModelVertex {
-            position: [0.0, height, length],
-            normal: [-1.0, 0.0, 0.0],
-        },
-        ModelVertex {
-            position: [0.0, height, 0.0],
-            normal: [-1.0, 0.0, 0.0],
-        },
+        ModelVertex::new([-1.0, 1.0, 1.0], [-1.0, 0.0, 0.0]),
+        ModelVertex::new([-1.0, 1.0, -1.0], [-1.0, 0.0, 0.0]),
+        ModelVertex::new([-1.0, -1.0, -1.0], [-1.0, 0.0, 0.0]),
+        ModelVertex::new([-1.0, -1.0, 1.0], [-1.0, 0.0, 0.0]),
+
         // Right
-        ModelVertex {
-            position: [width, 0.0, 0.0],
-            normal: [1.0, 0.0, 0.0],
-        },
-        ModelVertex {
-            position: [width, 0.0, length],
-            normal: [1.0, 0.0, 0.0],
-        },
-        ModelVertex {
-            position: [width, height, length],
-            normal: [1.0, 0.0, 0.0],
-        },
-        ModelVertex {
-            position: [width, height, 0.0],
-            normal: [1.0, 0.0, 0.0],
-        },
+        ModelVertex::new([1.0, 1.0, -1.0], [1.0, 0.0, 0.0]),
+        ModelVertex::new([1.0, 1.0, 1.0], [1.0, 0.0, 0.0]),
+        ModelVertex::new([1.0, -1.0, 1.0], [1.0, 0.0, 0.0]),
+        ModelVertex::new([1.0, -1.0, -1.0], [1.0, 0.0, 0.0]),
     ];
 
     let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
@@ -119,10 +49,10 @@ pub async fn load_block(device: &wgpu::Device, queue: &wgpu::Queue) -> anyhow::R
         contents: bytemuck::cast_slice(&vertices),
         usage: wgpu::BufferUsages::VERTEX,
     });
-    
+
     let indices = [
         // Top
-        0, 1, 2, 2, 3, 0,
+        0, 1, 2, 2, 3, 0, 
         // Bottom
         4, 5, 6, 6, 7, 4,
         // Front
@@ -133,6 +63,7 @@ pub async fn load_block(device: &wgpu::Device, queue: &wgpu::Queue) -> anyhow::R
         16, 17, 18, 18, 19, 16,
         // Right
         20, 21, 22, 22, 23, 20,
+
     ];
 
     let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
